18f9e1e53608558e46d010aa3d92e12a9a8c1f4a
Devansh Agrawal <devansh.b.agrawal@gmail.com>
2024-02-08 19:22:59 -0500

    Auto Progress

--- KitBot2024/src/main/java/frc/robot/Robot.java
+++ KitBot2024/src/main/java/frc/robot/Robot.java
@@ -20,9 +20,6 @@ import edu.wpi.first.wpilibj.Timer;
  * name of this class or
  * the package after creating this project, you must also update the
  * build.gradle file in the
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -32,7 +29,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
+  private static final String kSpeakerMid = "Speaker Middle";
+  private static final String kRedLongAuto = "Red Long Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
@@ -40,6 +38,7 @@ public class Robot extends TimedRobot {
   private final PWMSparkMax rightFront = new PWMSparkMax(2);
   private final PWMSparkMax leftBack = new PWMSparkMax(3);
   private final PWMSparkMax rightBack = new PWMSparkMax(4);
+
 
   PWMSparkMax m_frontLeft = new PWMSparkMax(1);
   PWMSparkMax m_rearLeft = new PWMSparkMax(2);
@@ -49,6 +48,9 @@ public class Robot extends TimedRobot {
   private final DifferentialDrive myDrive = new DifferentialDrive(m_frontLeft, m_frontRight);
 
   private final Timer timer1 = new Timer();
+
+  private final PWMSparkMax FeedWheel = new PWMSparkMax(5);
+  private final PWMSparkMax LaunchWheel = new PWMSparkMax(6);
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -61,7 +63,8 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
+    m_chooser.addOption("Speaker middle", kSpeakerMid);
+    m_chooser.addOption(name:"Red," kRedLongAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
     m_rearLeft.addFollower(m_frontLeft);
@@ -72,10 +75,10 @@ public class Robot extends TimedRobot {
     rightFront.setInverted(false);
     rightBack.setInverted(false);
 
-    timer1.start();
-    leftFront.setInverted(true);
-    leftBack.setInverted(true);
+    FeedWheel.setInverted(true);
+    LaunchWheel.setInverted(true);
 
+    timer1.start();
   }
 
   /**
@@ -117,19 +120,6 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
@@ -142,67 +132,43 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
+      case kRedLongAuto
+      
+       break;
+      case kSpeakerMid: // start middle speaker lauch & back up
+        if(timer1.get()< 1)
+        {
+          LaunchWheel.set(1);
+          
+        }
+        else if(timer1.get()< 1.75)
+        {
+          LaunchWheel.set(1);
+          FeedWheel.set(1);
+        }
+        else if(timer1.get()<5.0){
+          LaunchWheel.set(0);
+          FeedWheel.set(0);
+          myDrive.tankDrive(0, 0);
+        }
+        else{
+          LaunchWheel.set(0);
+          FeedWheel.set(0);
+        }
         break;
       case kDefaultAuto:
       default:
-        myDrive.tankDrive(.3, .3);
+        if(timer1.get() < 2.0)
+        {
+          myDrive.tankDrive(-.4, -.4);
+        }
+        else
+        {
+          myDrive.tankDrive(0, 0);
+        }
         break;
     }
   }
-  
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-  }
-
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {
-  }
-
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {
-  }
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {
-  }
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {
-  }
-}
-        leftFront.set(.5);
-        rightFront.set(.5);
-        leftBack.set(.5);
-        rightBack.set(.5);
-
-        
-        
-        break;
-    }
-  }
-
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {}
@@ -235,3 +201,4 @@ public class Robot extends TimedRobot {
   @Override
   public void simulationPeriodic() {}
 
+}
